651d5fda80a5ef686315857e3e172002fffd3b50
Alex Welch <me@alexwelch.com>
2014-09-23 13:05:56 -0600

    secure remote agent via BASIC AUTH and SSL [#79197598]

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,9 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :null_session
 
+  if Rails.env.production?
+    http_basic_authenticate_with name: PanamaxRemoteAgent::USERNAME,
+      password: PanamaxRemoteAgent::PASSWORD
+  end
+
 end
