e0c60d38af0363c57ddcf299c7760e33d784387c
Brian DeHamer <brian@dehamer.com>
2014-11-13 15:30:53 -0800

    Add global error handler
    Ensures that any errors raised in the agent are logged and returned consistently.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,4 +8,23 @@ class ApplicationController < ActionController::Base
       password: PanamaxRemoteAgent::PASSWORD
   end
 
+  rescue_from StandardError, with: :handle_exception
+
+  rescue_from Faraday::Error::ConnectionFailed do |ex|
+    handle_exception(ex, :adapter_connection_error)
+  end
+
+  def handle_exception(ex, message=nil, &block)
+    log_message = "\n#{ex.class} (#{ex.message}):\n"
+    log_message << "  " << ex.backtrace.join("\n  ") << "\n\n"
+    logger.error(log_message)
+
+    message = message.nil? ? ex.message : t(message, default: message.to_s)
+
+    block.call(message) if block_given?
+
+    unless performed?
+      render json: { message: message }, status: :internal_server_error
+    end
+  end
 end
